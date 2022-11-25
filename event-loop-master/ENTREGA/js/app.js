3542cf134f09bd9ac7ae44107f5c001d2c67c12b
G4z1m3r <garagonesl78@gmail.com>
2022-11-25 20:53:23 +0100

    log in

--- event-loop-master/ENTREGA/js/app.js
+++ event-loop-master/ENTREGA/js/app.js
@@ -4,21 +4,44 @@
   //SI son validos abrir ventana con el blog.html
   //NO son validos mostrar mensaje de error
 
-  const formulario = document.getElementsByClassName("form-signin")[0]
+const formulario = document.getElementsByClassName("form-signin")[0]
 
-  formulario.addEventListener("submit", evento =>{
-    evento.preventDefault()
+formulario.addEventListener("submit", evento =>{
+  evento.preventDefault()
   
-    logingFunction()
+  logingFunction()
     
-  })
+})
   
-  function logingFunction(){
-    const email = formulario.querySelector("#inputEmail")
-    const passwd = formulario.querySelector("#inputPassword")
+async function logingFunction(){
+  const email = formulario.querySelector("#inputEmail").value
+  const passwd = formulario.querySelector("#inputPassword").value
+  let checker = true  
+  const objeto = await (await fetch(`https://jsonplaceholder.typicode.com/users?email=${email}`)).json()
+  // if (!checkLoging){
+    console.log(objeto)
+      if (objeto.length==0){
+        checker=false
+      }
+      if (objeto[0].address.zipcode!=passwd){
+         checker=false
+      }
+      if (checker){
+        window.location.href="html/blog.html"
+      }
+  // }
+  
+}
+
+// async function getJSONByEmail(email){
+//   try{
     
-    if (!checkLoging){
-      
-    }
-  
-  }
+//     return objetousuario
+//   }catch(error) {
+//   	console.log("error")
+//   }
+ 
+// }
+
+
+// console.log(getJSONByEmail("Sincere@april.biz"))
