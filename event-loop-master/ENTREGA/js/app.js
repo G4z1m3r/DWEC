97eebce9ae680da0f8cad649a6e848f2b2a61785
G4z1m3r <garagonesl78@gmail.com>
2022-11-25 16:27:46 +0100

    prueba

--- event-loop-master/ENTREGA/js/app.js
+++ event-loop-master/ENTREGA/js/app.js
@@ -1,6 +1,24 @@
 //PARTE 1
-
 //capturar los datos de entrada del formulario
 // enviar a ver si son validos en la API
   //SI son validos abrir ventana con el blog.html
   //NO son validos mostrar mensaje de error
+
+  const formulario = document.getElementsByClassName("form-signin")[0]
+
+  formulario.addEventListener("submit", evento =>{
+    evento.preventDefault()
+  
+    logingFunction()
+    
+  })
+  
+  function logingFunction(){
+    const email = formulario.querySelector("#inputEmail")
+    const passwd = formulario.querySelector("#inputPassword")
+    
+    if (!checkLoging){
+      
+    }
+  
+  }
