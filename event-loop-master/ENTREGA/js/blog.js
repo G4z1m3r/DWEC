5a6167ff86f3cf34bb991e07cc3d1f621bfadf33
Gonzalo Aragonés López <garagonesl78@gmail.com>
2022-11-29 15:02:06 +0100

    paginacion simplev1

--- event-loop-master/ENTREGA/js/blog.js
+++ event-loop-master/ENTREGA/js/blog.js
@@ -84,24 +84,76 @@ async function mostrarPosts() {
     divListaPosts.style.display = 'flex'
     divListaPosts.style.flexWrap = 'wrap'
 
-    posts.forEach(post =>{
-        let cardbody = document.createElement('div')
-        cardbody.classList.add('card-body')
+    var current_page = 1;
+    var obj_per_page = 5;
+    function totNumPages()
+    {
+        return Math.ceil(posts.length / obj_per_page);
+    }
 
-        let cardtitle = document.createElement('h5')
-        cardtitle.classList.add('card-title')
-        let titletext = document.createTextNode(post.title)
-        cardtitle.appendChild(titletext)
-        cardbody.appendChild(cardtitle)
+    function prevPage()
+    {
+        if (current_page > 1) {
+            current_page--;
+            change(current_page);
+        }
+    }
+    function nextPage()
+    {
+        if (current_page < totNumPages()) {
+            current_page++;
+            change(current_page);
+        }
+    }
 
-        let cardtext = document.createElement('p')
-        cardtext.classList.add('card-text')
-        let texttext = document.createTextNode(post.body)
-        cardtext.appendChild(texttext)
-        cardbody.appendChild(cardtext)
+    function change(page)
+    {
+        var btn_next = document.getElementById("btn_next");
+        var btn_prev = document.getElementById("btn_prev");
+        var listing_table = document.getElementById("TableList");
+        var page_span = document.getElementById("page");
+        if (page < 1) page = 1;
+        if (page > totNumPages()) page = totNumPages();
+        listing_table.innerHTML = "";
+        for (var i = (page-1) * obj_per_page; i < (page * obj_per_page); i++) {
+            listing_table.innerHTML += obj[i].number + "<br>";
+        }
+        page_span.innerHTML = page;
+        if (page == 1) {
+            btn_prev.style.visibility = "hidden";
+        } else {
+            btn_prev.style.visibility = "visible";
+        }
+        if (page == totNumPages()) {
+            btn_next.style.visibility = "hidden";
+        } else {
+            btn_next.style.visibility = "visible";
+        }
+    }
+    window.onload = function() {
+        change(1);
+    };
+    
 
-        divListaPosts.appendChild(cardbody)
-    })
+
+    // posts.forEach(post =>{
+    //     let cardbody = document.createElement('div')
+    //     cardbody.classList.add('card-body')
+
+    //     let cardtitle = document.createElement('h5')
+    //     cardtitle.classList.add('card-title')
+    //     let titletext = document.createTextNode(post.title)
+    //     cardtitle.appendChild(titletext)
+    //     cardbody.appendChild(cardtitle)
+
+    //     let cardtext = document.createElement('p')
+    //     cardtext.classList.add('card-text')
+    //     let texttext = document.createTextNode(post.body)
+    //     cardtext.appendChild(texttext)
+    //     cardbody.appendChild(cardtext)
+
+    //     divListaPosts.appendChild(cardbody)
+    // })
 
 }
 
